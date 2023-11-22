bbfaf85683e99321d655da2ad78796a7e844fc29
MasakiAraki <kgmsk411@gmail.com>
2023-11-22 17:10:29 +0900

    fix: fixed logout bug

---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "turbo", to: "turbo.min.js", preload: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: :show
   root to: 'welcome#index'
   devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  resources :users, only: :show
 end
