f7c253f626f89225fc0d4aeeb35da93f44ca68bc
MasakiAraki <kgmsk411@gmail.com>
2025-01-31 22:08:50 +0900

    refactor: remove work records functionality and associated files

--- app/models/work_record.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-# This class is used to manage the work record.
-class WorkRecord < ApplicationRecord
-  belongs_to :user
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   devise_for :users
-  resources :users, only: :show do
-    resources :work_records, only: %i[index new]
-  end
-
-  post '/users/:user_id/work_records', to: 'work_records#create', as: :create_work_record
+  resources :users, only: %i[show]
 end
