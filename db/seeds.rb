7c3dc683527298fbe36d8e6e87ec89de81451ae4
MasakiAraki <kgmsk411@gmail.com>
2025-02-02 23:13:56 +0900

    feat: add user factory and seed data for development environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+if Rails.env.development?
+  require 'faker'
+  include FactoryBot::Syntax::Methods
+
+  puts 'creating admins'
+
+  create(
+    :user,
+    first_name: 'Masaki',
+    last_name: 'Araki',
+    email: 'araki@example.com',
+    password: 'password'
+  )
+
+  puts 'creating users'
+
+  5.times do
+    create(:user, :filled)
+  end
+end
